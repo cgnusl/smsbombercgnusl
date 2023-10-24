80cd02cc3885798413caf21b0993029436f669f2
cgnusl <121693611+cgnusl@users.noreply.github.com>
2023-10-24 19:53:57 +0300

    Update title.js

--- modules/title.js
+++ modules/title.js
@@ -1,8 +1,8 @@
 function console_title(x) {
     if (process.platform == 'win32') {
-        process.title = x + " / FastBomber v1.0 - github.com/fastuptime";
+        process.title = x + " / CGNUSL v1.2 - github.com/cgnusl";
     } else {
-        process.stdout.write('\x1b]2;' + x + " / FastBomber v1.0 - github.com/fastuptime" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " İf you have a problem contact from dc cgnusl/Bir sorun bulduysanız dcden ulaşın cgnusl" + '\x1b\x5c');
     }
 }
 
